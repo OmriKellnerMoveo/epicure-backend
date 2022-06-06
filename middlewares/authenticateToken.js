d6fcab08bc0c39a31b2409cb69a9bbc407700c25
OmriKellnerMoveo <omrik@moveo.co.il>
2022-06-06 20:17:58 +0300

    fix auth

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -1,24 +1,15 @@
-const jwt =require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-exports.authenticateToken = (
-    req,
-    res,
-    next
-) => {
+exports.authenticateToken = (req, res, next) => {
     const authHeader = req.headers["authorization"];
-    const accessToken  =
-        authHeader && authHeader?.split(" ")[1];//second arg = token
+    const accessToken = authHeader && authHeader;
     if (accessToken == null) return res.sendStatus(401);
-    jwt.verify(
-        accessToken,
-        process.env.TOKEN_SECRET,
-        (err, email) => {
-            if (err) {
-                console.log(authHeader);
-                return res.sendStatus(403);
-            }
-            req.body.email = email;
-            next();
+    jwt.verify(accessToken, process.env.TOKEN_SECRET, (err, email) => {
+        if (err) {
+            console.log(authHeader);
+            return res.sendStatus(403);
         }
-    );
+        req.body.email = email;
+        next();
+    });
 };
